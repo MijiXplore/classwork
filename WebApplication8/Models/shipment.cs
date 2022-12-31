e2252791360ab7cc6298941ea237bd35398c282a
test <shittuolamijiu@gmail.com>
2022-12-30 18:55:58 -0700

    class Exam for coding question submission

---
+++ WebApplication8/Models/shipment.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication8.Models
+{
+    public class shipment
+    {
+        public string orderNumber { get; set; }
+        public string customerName { get; set; }
+        public int orderQty { get; set; }
+
+        public shipment()
+        {
+
+        }
+
+        public shipment(string orderNumber, string customerName, int orderQty)
+        {
+            this.orderNumber = orderNumber;
+            this.customerName = customerName;
+            this.orderQty = orderQty;
+        }
+    }
+
+    
+}
